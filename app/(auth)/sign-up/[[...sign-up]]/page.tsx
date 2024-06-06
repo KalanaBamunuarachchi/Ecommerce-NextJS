5763f82b6ac99bc1ce9c281fa1f69d078d250607
kalanabamunuarachchi <kalana.bamunuarachchi@outlook.com>
2024-06-06 13:59:27 +0530

    login and signup

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+  <div className="h-screen flex justify-center items-center">
+    <SignUp />
+  </div>);
 }
