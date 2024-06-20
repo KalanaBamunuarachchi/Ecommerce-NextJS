81325dce007caa1ea420357449a6d42974cfe0e2
KalanaBamunuarachchi <kalana.bamunuarachchi@outlook.com>
2024-06-20 16:54:03 +0530

    .

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LeftSideBar = () => {
   return (
-    <div className='h-screen left-0 top-0 stick '>LeftSideBar</div>
+    <div className='h-screen left-0 top-0 stick p-10 flex flex-col gap-16'>LeftSideBar</div>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+
+        "white-1": ""
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
